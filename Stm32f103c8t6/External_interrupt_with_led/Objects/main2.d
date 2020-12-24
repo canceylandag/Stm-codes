d8a704acad5deab750cedf1cb95e087a987a9200
canceylandag <69530509+canceylandag@users.noreply.github.com>
2020-12-24 20:36:10 +0300

    Create main2.d

---
+++ Stm32f103c8t6/External_interrupt_with_led/Objects/main2.d
@@ -0,0 +1,16 @@
+./objects/main2.o: main2.c \
+  D:\Programs\KeIL\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h \
+  RTE\_Target_1\RTE_Components.h \
+  D:\Programs\KeIL\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm3.h \
+  D:\Programs\KeIL\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  D:\Programs\KeIL\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
+  D:\Programs\KeIL\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
+  D:\Programs\KeIL\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
+  D:\Programs\KeIL\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  D:\Programs\KeIL\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  D:\Programs\KeIL\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h \
+  RTE\Device\STM32F103C8\stm32f10x_conf.h \
+  D:\Programs\KeIL\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_exti.h \
+  D:\Programs\KeIL\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h \
+  D:\Programs\KeIL\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h \
+  D:\Programs\KeIL\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\misc.h
